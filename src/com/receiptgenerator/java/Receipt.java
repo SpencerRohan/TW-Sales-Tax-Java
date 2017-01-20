108efa9e8b4fd9568fbc6d47949558b7b86bc032
Spencer Rohan <spencermrohan@gmail.com>
2017-01-19 20:04:36 -0600

    ADD ReceiptScanner Model

--- src/com/receiptgenerator/java/Receipt.java
+++ src/com/receiptgenerator/java/Receipt.java
@@ -1,12 +1,23 @@
 package com.receiptgenerator.java;
 
+import com.receiptgenerator.java.model.ReceiptScanner;
+
 import java.io.IOException;
 
 /**
  * Created by N/A on 1/19/17.
  */
 public class Receipt {
-    public static void main(String[] args) throws IOException {
-        System.out.println("Here");
+    public static void main(String[] importCarts) throws IOException {
+        String[] defaultCarts = {
+                "assets/txt/input1.txt",
+                "assets/txt/input2.txt",
+                "assets/txt/input3.txt"
+        };
+        String[] queuedCartFiles = (importCarts.length > 0) ?
+                importCarts : defaultCarts;
+        ReceiptScanner rScanner = new ReceiptScanner(queuedCartFiles);
+
+
     }
 }
---
+++ src/com/receiptgenerator/java/model/ReceiptScanner.java
@@ -0,0 +1,15 @@
+package com.receiptgenerator.java.model;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+/**
+ * Created by N/A on 1/19/17.
+ */
+public class ReceiptScanner {
+    public ReceiptScanner(String[] queuedCarts) {
+        for (String cart : queuedCarts) {
+            
+        }
+    }
+}
