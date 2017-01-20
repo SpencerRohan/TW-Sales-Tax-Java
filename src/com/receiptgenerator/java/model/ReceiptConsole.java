6ed3183f0d8899da7471cdb3c35677e3180055d2
Spencer Rohan <spencermrohan@gmail.com>
2017-01-20 12:27:59 -0600

    FINISH Display Console

--- src/com/receiptgenerator/java/model/ReceiptConsole.java
+++ src/com/receiptgenerator/java/model/ReceiptConsole.java
@@ -1,9 +1,10 @@
 package com.receiptgenerator.java.model;
 
 public class ReceiptConsole {
-    private String itemFormat = "%1$-40s %2$10.2f %n";
-    private String totalFormat = "%1$40s %2$10.2f %n";
-    private String lineFormat = "%1$40s %2$10.2f %n";
+    private String itemFormat = "%1$-40s %2$6.2f %n";
+    private String taxFormat = "%1$40s %2$6.2f %n";
+    private String totalFormat = "%1$40s %2$6.2f %n%n%n";
+    private String lineFormat = "%1$40s %n";
 
     public void purchaseList(Item item) {
         System.out.format(itemFormat, item.getQty() + " " + item.getDetails() +": ", item.getAfterTax());
@@ -11,9 +12,7 @@ public class ReceiptConsole {
 
 
     public void salesTax(Double taxTotal) {
-        System.out.format(totalFormat, "Sales Taxes:", taxTotal);
-//        System.out.printf("Sales Taxes" +": ");
-//        System.out.printf("%.2f" + "\n", taxTotal);
+        System.out.format(taxFormat, "Sales Taxes:", taxTotal);
     }
 
     public void totalSale(Double saleTotal) {
@@ -24,5 +23,7 @@ public class ReceiptConsole {
     }
 
     public void lineBreak() {
+        String dashes = new String(new char[48]).replace("\0", "-");
+        System.out.format(dashes+"%n");
     }
 }
