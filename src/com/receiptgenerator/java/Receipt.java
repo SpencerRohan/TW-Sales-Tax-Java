9f0775c59d570f7661f7155b83da796b4a59a694
Spencer Rohan <spencermrohan@gmail.com>
2017-01-19 19:21:13 -0600

    APP appears to be running

--- src/com/receiptgenerator/java/Receipt.java
+++ src/com/receiptgenerator/java/Receipt.java
@@ -1,8 +1,12 @@
 package com.receiptgenerator.java;
 
+import java.io.IOException;
+
 /**
  * Created by N/A on 1/19/17.
  */
 public class Receipt {
-
+    public static void main(String[] args) throws IOException {
+        System.out.println("Here");
+    }
 }
