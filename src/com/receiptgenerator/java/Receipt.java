7878527e0b8dea0872d0a4ce316fc5664cdcf60a
Spencer Rohan <spencermrohan@gmail.com>
2017-01-19 19:19:19 -0600

    ADD basic folder structure

---
+++ src/com/receiptgenerator/java/Receipt.java
@@ -0,0 +1,8 @@
+package com.receiptgenerator.java;
+
+/**
+ * Created by N/A on 1/19/17.
+ */
+public class Receipt {
+
+}
---
+++ tests/com/receiptgenerator/java/ReceiptTest.java
@@ -0,0 +1,10 @@
+package com.receiptgenerator.java;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by N/A on 1/19/17.
+ */
+public class ReceiptTest {
+
+}
